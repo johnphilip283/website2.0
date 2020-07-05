2d161ae42b3f8a486bb2afa5dfa4d3a01b0f0719
John Philip <john.philip@buzzfeed.com>
2020-07-04 22:58:06 -0400

    Updates

---
+++ precache-manifest.229c46516fecbec7a67666fb9fadc7d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0424b6972606ecc7f581402bb24971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fabaf96407b746a89d3",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "86e3d9f6a8f26a298bd7",
+    "url": "/static/css/main.342e60ba.chunk.css"
+  },
+  {
+    "revision": "3fabaf96407b746a89d3",
+    "url": "/static/js/2.d49ae233.chunk.js"
+  },
+  {
+    "revision": "86e3d9f6a8f26a298bd7",
+    "url": "/static/js/main.a941b524.chunk.js"
+  },
+  {
+    "revision": "3989be3e314f5121b19a",
+    "url": "/static/js/runtime~main.df838c8c.js"
+  }
+]);
