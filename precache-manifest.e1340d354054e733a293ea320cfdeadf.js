3e09a2176c862422e4165fc69d3d448f91d74ae7
John Philip <jphilip@spotify.com>
2025-01-13 02:06:19 -0500

    Updates

---
+++ precache-manifest.e1340d354054e733a293ea320cfdeadf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f700ea16f01cba0559f2043e460c46f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abf49bda85912dcc9d66",
+    "url": "/static/css/2.4e15b5ff.chunk.css"
+  },
+  {
+    "revision": "48c6de7ded9f902e4212",
+    "url": "/static/css/main.ae044596.chunk.css"
+  },
+  {
+    "revision": "abf49bda85912dcc9d66",
+    "url": "/static/js/2.bbabe7c9.chunk.js"
+  },
+  {
+    "revision": "48c6de7ded9f902e4212",
+    "url": "/static/js/main.d058f33b.chunk.js"
+  },
+  {
+    "revision": "5b4c63c12ad7a6bc59d6",
+    "url": "/static/js/runtime~main.ded9b2ef.js"
+  }
+]);
